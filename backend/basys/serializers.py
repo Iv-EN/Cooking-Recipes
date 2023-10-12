39d42d5afc15787868cb52e8dbd99cf575474f2b
Iv-EN <engenivanov@yandex.ru>
2023-10-12 21:03:15 +0300

    Fixed isort errors

--- backend/basys/serializers.py
+++ backend/basys/serializers.py
@@ -10,7 +10,6 @@ from rest_framework.validators import UniqueValidator
 
 from recipes.models import AmountIngredient, Ingredient, Recipe, Tag
 from users.models import Follow
-
 from .utils import recipe_ingredient_create
 
 User = get_user_model()
