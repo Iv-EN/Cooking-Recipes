d85cfe895fef14614d5b7f88d367df23978abec9
Iv-EN <engenivanov@yandex.ru>
2023-12-11 09:40:47 +0300

    Добавил загрузку ингредиентов и тегов

--- backend/api/management/commands/importcsv.py
+++ backend/api/management/commands/importcsv.py
@@ -1,72 +1,49 @@
 import csv
-import os
 
-from django.apps import apps
-from django.conf import settings
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient, Tag
+
+NAME_MODEL_FILE = {
+    'ingredient': (Ingredient, 'ingredients.csv'),
+    'tag': (Tag, 'tag.csv'),
+}
 
 
 class Command(BaseCommand):
-    help = (
-        'This command helps you to fill CSV data into db. '
-        'To perform CSV data migration type the following command: '
-        '>>> python manage.py importcsv '
-        '--filename "<filename>.csv" '
-        '--model_name "<model_name>" '
-        '--app_name "<app_name>"'
-    )
+    '''Загрузка данных из csv файлов в модели.'''
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            '--filename',
-            type=str,
-            help='file name (with ".csv" extension)'
-        )
-        parser.add_argument(
-            '--model_name',
-            type=str,
-            help='model name that is used with this ".csv" file'
-        )
-        parser.add_argument(
-            '--app_name',
-            type=str,
-            help='django app name'
+    @staticmethod
+    def clear_model(model):
+        model.objects.all().delete()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def load_model(self, model_name, field_names):
+        model, file_path = NAME_MODEL_FILE.get(model_name)
+        with open(self.get_csv_file(file_path), encoding='utf-8') as file:
+            reader = csv.reader(file, delimiter=',')
+            self.clear_model(model)
+            line = 0
+            for row in reader:
+                if row != '' and line > 0:
+                    params = dict(zip(field_names, row))
+                    _, created = model.objects.get_or_create(**params)
+                line += 1
+
+    def load_ingredient(self):
+        self.load_model(
+            'ingredient',
+            ['name', 'measurement_unit']
         )
 
-    def get_csv_file(self, filename):
-        file_path = os.path.join(
-            settings.BASE_DIR,
-            'data',
-            filename
+    def load_tag(self):
+        self.load_model(
+            'tag',
+            ['name', 'color', 'slug']
         )
-        return file_path
 
-    def handle(self, *args, **options):
-        filename = options['filename']
-        app_name = options['app_name']
-        model_name = options['model_name']
-        file_path = self.get_csv_file(filename)
-        self.stdout.write(self.style.SUCCESS(f'Reading: {file_path}'))
-        _model = apps.get_model(app_name, model_name)
-        _model.objects.all().delete()
-        try:
-            with open(file_path, 'r', encoding='utf-8') as csv_file:
-                reader = csv.reader(csv_file, delimiter=',')
-                bulk_create_data = (
-                    _model(name=row[0], measurement_unit=row[1])
-                    for row in reader
-                )
-                if 'tags.csv' in filename:
-                    bulk_create_data = (
-                        _model(name=row[0], color=row[1], slug=row[2])
-                        for row in reader
-                    )
-                _model.objects.bulk_create(bulk_create_data)
-                line_count = _model.objects.count()
-            self.stdout.write(
-                self.style.SUCCESS(
-                    f'{line_count} entries added to {model_name}'
-                )
-            )
-        except FileNotFoundError:
-            raise CommandError(f'File {file_path} does not exist')
+    def handle(self, *args, **kwargs):
+        self.load_ingredient()
+        self.load_tag()
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,5 +5,6 @@ set -x
 python manage.py migrate --noinput || exit 1
 python manage.py collectstatic --noinput || exit 1
 cp -r /app/static/. /static/ || exit 1
+python manage.py importcsv
 
 exec "$@"
