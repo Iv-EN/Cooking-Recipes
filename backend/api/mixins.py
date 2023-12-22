517b6b5b72fec970a521568473c99244ae87071e
Iv-EN <engenivanov@yandex.ru>
2023-12-23 01:30:28 +0300

    исправил ошибки flake8

--- backend/api/mixins.py
+++ backend/api/mixins.py
@@ -1,5 +1,4 @@
 from django.db.models import Model, Q
-from django.db.utils import IntegrityError
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer
